42b482ce617f8944c332e46ef64ffc54cb348449
Felps <jfelipesmarques@gmail.com>
2026-04-21 16:49:47 -0300

    .

--- src/pages/hero-selector/HeroSelector.tsx
+++ src/pages/hero-selector/HeroSelector.tsx
@@ -1,7 +1,9 @@
 import HeroDisplay from "./components/Heroes";
+import MenuOptions from "../menu/components/menuList";
 
 export default function HeroSelector() {
 	return (
+
 			<HeroDisplay />
 	);
 }
